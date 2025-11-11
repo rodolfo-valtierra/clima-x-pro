988f3ae158104b1c7d775355b4a815ebb1562bad
Rodolfo valtierra <rivd@outlook.es>
2025-11-10 22:40:23 -0600

    adding carousel

--- src/Components/Title.svelte
+++ src/Components/Title.svelte
@@ -1,8 +1,8 @@
 <script >
 	import Logo from "$lib/logo-X.PNG"
-	let {text} = $props();
+	let {text, size=30} = $props();
 </script>
 <div class="flex justify-center">
 	<img class="xl:h-16 h-12" src={Logo} alt="Logo X">
-	<h1 class="xl:text-[32pt] text-[22pt]">{text}</h1>
+	<h1 class={[`lg:text-[${size}pt] text-[${size-10}pt]`]}>{text}</h1>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,10 @@
 <script>
 	import Hero from '$hero/Hero.svelte'
 	import Services from '$hero/Services.svelte'
+	import Images from '$hero/Images.svelte'
+
 </script>
 
 <Hero/>
 <Services/>
+<Images/>
