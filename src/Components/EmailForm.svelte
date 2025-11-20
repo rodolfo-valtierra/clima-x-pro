0274aeaa12e8e8df15c5105ddbe33004a5f97d8f
Rodolfo valtierra <rivd@outlook.es>
2025-11-19 23:41:40 -0600

    changin footer style

--- src/Components/EmailForm.svelte
+++ src/Components/EmailForm.svelte
@@ -2,28 +2,34 @@
 	import ServicesList from '$utils/services.js'
 </script>
 
-<div class="xl:order-last order-first grid  m-2 text-white place-center items-center gap-3 md:h-76">
-	<h1 class="col-span-2 text-4xl">Contactanos y cotiza con nosotros</h1>
-	<div class="col-start-1 grid-fields gap-5 content-start">
+<div class="xl:order-first order-first grid m-2 mb-8 text-white place-center items-center gap-3 md:h-76 w-200">
+	<h1 class="col-span-2 text-4xl">Contactanos al (477-284-6535) o cotiza con nosotros</h1>
+	<div class="grid grid-col-2 col-start-1 grid-fields gap-5 content-start">
 		<div class="flex flex-col col-start-1">
 			<label for="email">Correo / Email</label>
-			<input type="" name="email" placeholder="Email"/>
+			<input name="email" placeholder="Email"/>
 		</div>
-		<div class="flex flex-col col-start-1">
+    <div class="flex flex-col col-start-2">
 			<label for="name">Nombre</label>
-			<input type="" name="name" value="" placeholder="Name"/>
+			<input name="name" value="" placeholder="Nombre"/>
 		</div>
 		<div class="flex flex-col col-start-1">
+			<label for="telefono">Telefono</label>
+			<input name="telefono" placeholder="Telefono"/>
+		</div>
+		<div class="flex flex-col col-start-2">
 			<label for="service">Selecciona un servicio</label>
-			<select  name="service" value="" placeholder="Servicio requerido ">
+			<select  name="service" value="-1" >
+        <option disabled hidden value="-1" >Nuestros servicios...</option>
 				{#each ServicesList as service, i}
 					<option value={i}>{service}</option>
 				{/each}
+					<option>Otro...</option>
 			</select>
 		</div>	
-    <div class="flex flex-col col-start-2 ">
+    <div class="flex flex-col col-span-2 ">
       <label for="message">Mensage</label>
-      <textarea name="message" rows="3" cols="30" placeholder="Describe the work you need"></textarea>
+      <textarea name="message" rows="4" cols="30" placeholder="Describe el servicio aqui..."></textarea>
     </div>
 	</div>
 </div>
@@ -34,23 +40,7 @@
 		color: black;
 		font-size: 12pt;
 		padding: 6px;
-		border-radius: 5px;
+		border-radius: 10px;
 	}
 
-  .grid-fields {
-    display: grid;
-    grid-template: repeat(3, 50px) / repeat(2, 1fr);
-  }
-
-  .grid-fields > div:nth-last-child(1){
-    grid-area:  1 / 2 / auto / 2;
-    height: fit-content;
-  }
-
-  @media (width < 740px) {
-    .grid-fields {
-      display: flex;
-      flex-direction: column;
-      }
-    }
 </style>
